b541a4905925c76e0fe0a6332363a2b806acc8ed
tirthpatel1020 <46437287+tirthpatel1020@users.noreply.github.com>
2026-08-31 18:43:14 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "tokebook-v3";
+const CACHE = "tokebook-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json",
   "./apple-touch-icon.png", "./icon-192.png", "./icon-512.png"];
 
